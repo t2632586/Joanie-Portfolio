08ea4aebc310a66052c70fa7de41e528573c39df
Joanie <joanie0324@gmail.com>
2021-03-13 21:43:19 +0800

    修改

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -11,14 +11,14 @@ $(document).ready(function () {
   $(window).scroll(function () {
     var scrollPos = $(window).scrollTop();
     var windowHeight = $(window).height();
-    console.log(scrollPos, windowHeight);
+    // console.log(scrollPos, windowHeight);
 
 
     $('.scrollTop').each(function () {
       var target = $(this).attr('href');
       var targetPos = $(target).offset().top;
       var targetHeight = $(target).outerHeight();
-      if (targetPos <= scrollPos && (targetPos + targetHeight) > scrollPos) {
+      if (targetPos - 1 <= scrollPos && (targetPos + targetHeight) > scrollPos) {
         $('.scrollTop').removeClass('active')
         $(this).addClass('active');
       } else {
